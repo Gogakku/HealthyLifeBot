ffc7e379104562020681f4b3250129ae0ee5500e
Gogakku <130288934+Gogakku@users.noreply.github.com>
2025-04-24 21:50:15 +1000

    Update HealthyLifeBot.kt

--- src/main/kotlin/org/example/HealthyLifeBot.kt
+++ src/main/kotlin/org/example/HealthyLifeBot.kt
@@ -328,4 +328,7 @@ class HealthyLifeBot : TelegramLongPollingBot() {
         response.replyMarkup = keyboard
         execute(response)
     }
+override fun clearWebhook() {
+    // Ничего не делаем, чтобы избежать удаления webhook и краша
+}
 }
